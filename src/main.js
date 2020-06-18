aa3d90b09b105a30454035e6ca19146bd14a8730
BanariVlad <banari.vlad@yandex.com>
2020-06-18 21:41:22 +0300

    Fixed zero

--- src/main.js
+++ src/main.js
@@ -8,10 +8,12 @@ Vue.directive("only-numbers", {
   bind(el) {
     let hasDot = false;
 
-    el.addEventListener("input" , (event) => {
+    el.addEventListener("input", (event) => {
       let formattedValue = event.target.value;
       let lastIntroduced = formattedValue[formattedValue.length - 1];
-      let valueWithoutLast = formattedValue.slice (0, -1);
+      let valueWithoutLast = formattedValue.slice(0, -1);
+
+      console.log(valueWithoutLast[valueWithoutLast.length - 1])
 
       if (el.value.length === 1 && lastIntroduced === "-") {
         el.value = lastIntroduced;
@@ -21,10 +23,13 @@ Vue.directive("only-numbers", {
       } else if (lastIntroduced === "." && formattedValue.length !== 1 && !hasDot) {
         el.value = formattedValue;
         hasDot = true;
+      } else if (lastIntroduced === "0" && valueWithoutLast[valueWithoutLast.length - 1] === "." ||
+        lastIntroduced === "0" && valueWithoutLast[valueWithoutLast.length - 1] === "0") {
+        el.value = formattedValue;
       } else if (lastIntroduced === " " || lastIntroduced === ".") {
         el.value = valueWithoutLast;
       } else if (isNaN(lastIntroduced) && lastIntroduced !== ".") {
-        el.value = formattedValue.slice(0, -1);
+        el.value = valueWithoutLast;
       } else {
         const result = parseFloat(formattedValue);
         el.value = isNaN(result) ? "" : parseFloat(formattedValue);
