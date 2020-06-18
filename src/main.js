92eb100381bb571f75cb163346de5d5a45485d12
BanariVlad <banari.vlad@yandex.com>
2020-06-18 21:25:29 +0300

    Backspace and zero doesn't work correctly

--- src/main.js
+++ src/main.js
@@ -4,6 +4,35 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+Vue.directive("only-numbers", {
+  bind(el) {
+    let hasDot = false;
+
+    el.addEventListener("input" , (event) => {
+      let formattedValue = event.target.value;
+      let lastIntroduced = formattedValue[formattedValue.length - 1];
+      let valueWithoutLast = formattedValue.slice (0, -1);
+
+      if (el.value.length === 1 && lastIntroduced === "-") {
+        el.value = lastIntroduced;
+      } else if (hasDot && !formattedValue.includes(".")) {
+        el.value = valueWithoutLast;
+        hasDot = false;
+      } else if (lastIntroduced === "." && formattedValue.length !== 1 && !hasDot) {
+        el.value = formattedValue;
+        hasDot = true;
+      } else if (lastIntroduced === " " || lastIntroduced === ".") {
+        el.value = valueWithoutLast;
+      } else if (isNaN(lastIntroduced) && lastIntroduced !== ".") {
+        el.value = formattedValue.slice(0, -1);
+      } else {
+        const result = parseFloat(formattedValue);
+        el.value = isNaN(result) ? "" : parseFloat(formattedValue);
+      }
+    });
+  }
+});
+
 new Vue({
   router,
   render: h => h(App)
