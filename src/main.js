72f9f1b350c667b80371b12094bae162d2f6ff9c
BanariVlad <banari.vlad@yandex.com>
2020-06-19 14:38:42 +0300

    Dot + number + backspace remained

--- src/main.js
+++ src/main.js
@@ -7,37 +7,46 @@ Vue.config.productionTip = false;
 Vue.directive("only-numbers", {
   bind(el) {
     let hasDot = false;
+    let hasMinus = false;
 
     el.addEventListener("input", (event) => {
       let formattedValue = event.target.value;
       let lastIntroduced = formattedValue[formattedValue.length - 1];
       let valueWithoutLast = formattedValue.slice(0, -1);
       let ifMinus = el.value.length === 1 && lastIntroduced === "-";
-      let firstDot = lastIntroduced === "." && formattedValue.length !== 1 && !hasDot;
-      let zeroAfterDot = lastIntroduced === "0" && hasDot;
-      let zeroAfterZero = lastIntroduced === "0" && valueWithoutLast[valueWithoutLast.length - 1] === "."
-        || lastIntroduced === "0" && valueWithoutLast[valueWithoutLast.length - 1] === "0";
-      let notDoubleDot = hasDot && !formattedValue.includes(".");
-      let notSpaceOrDot = lastIntroduced === " " || lastIntroduced === ".";
-      let antiDotOrSpace = isNaN(lastIntroduced) && lastIntroduced !== ".";
+      let ifFirstDot = formattedValue.length === 1 && lastIntroduced === ".";
+      let ifDotAfterMinus = lastIntroduced === "." && formattedValue.length === 2 && hasMinus;
 
-      if (ifMinus) {
-        el.value = lastIntroduced;
-      } else if (notDoubleDot) {
-        el.value = valueWithoutLast;
+      if (!formattedValue.includes(".")) {
         hasDot = false;
-      } else if (firstDot || zeroAfterDot || zeroAfterZero) {
+      } else if (ifMinus) {
+        el.value = lastIntroduced;
+        hasMinus = true;
+      } else if (ifFirstDot || ifDotAfterMinus) {
+        el.value = valueWithoutLast;
+      } else if (lastIntroduced === "." && !hasDot) {
         el.value = formattedValue;
         hasDot = true;
-      } else if (notSpaceOrDot || antiDotOrSpace) {
+      } else if (isNaN(lastIntroduced) || lastIntroduced === " ") {
         el.value = valueWithoutLast;
-      } else {
-        const result = parseFloat(formattedValue);
-        el.value = isNaN(result) ? "" : parseFloat(formattedValue);
       }
     });
   }
 });
+
+// else if (notDoubleDot) {
+//   el.value = valueWithoutLast;
+//   hasDot = false;
+// }
+// else if (firstDot || zeroAfterDot || zeroAfterZero) {
+//   el.value = formattedValue;
+//   hasDot = true;
+// } else if (notSpaceOrDot || antiDotOrSpace) {
+//   el.value = valueWithoutLast;
+// } else {
+//   const result = parseFloat(formattedValue);
+//   el.value = isNaN(result) ? "" : parseFloat(formattedValue);
+// }
 
 new Vue({
   router,
