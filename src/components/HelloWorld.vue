76e9eb3dcd79cdc48dbf81afdf259e4621363181
BanariVlad <banari.vlad@yandex.com>
2020-06-22 21:47:54 +0300

    Very beautiful

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,41 @@
 <template>
   <div class="hello">
-    <input type="text" v-only-numbers placeholder="Yep">
+    <input type="text" v-only-numbers placeholder="Yep" />
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
-  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- src/main.js
+++ src/main.js
@@ -6,40 +6,125 @@ Vue.config.productionTip = false;
 
 Vue.directive("only-numbers", {
   bind(el) {
-    let hasDot = false;
+    let newValue;
+    let oldValue;
+    let lastIntroduced;
 
-    el.addEventListener("input", (event) => {
-      let formattedValue = event.target.value;
-      let lastIntroduced = formattedValue[formattedValue.length - 1];
-      let valueWithoutLast = formattedValue.slice(0, -1);
-      let ifMinus = el.value.length === 1 && lastIntroduced === "-";
-      let ifFirstDot = formattedValue.length === 1 && lastIntroduced === ".";
-      let ifDotAfterMinus = lastIntroduced === "." && valueWithoutLast[valueWithoutLast.length - 1] === "-";
-      let lastIsDot = formattedValue[formattedValue.length - 1] === "." && hasDot;
-      let ifDoubleDot = lastIntroduced === "." && valueWithoutLast.includes(".");
+    el.addEventListener("keydown", event => {
+      let lastValue = event.key;
+      lastIntroduced = lastValue;
+      let zeroAndMinusValidation = validZeroAndMinus(lastValue);
+      oldValue = el.value;
 
-      if (!formattedValue.includes(".")) {
-        hasDot = false;
+      if (validFirstSymbols(lastValue) && lastValue !== "Backspace") {
+        return event.preventDefault();
       }
 
-      if (ifMinus) {
-        el.value = lastIntroduced;
-      } else if (lastIsDot) {
-        el.value = formattedValue;
-        hasDot = true;
-      } else if (ifFirstDot || ifDotAfterMinus) {
-        el.value = valueWithoutLast;
-      } else if (lastIntroduced === "." && !hasDot) {
-        el.value = formattedValue;
-        hasDot = true;
-      } else if (isNaN(lastIntroduced) || lastIntroduced === " ") {
-        el.value = valueWithoutLast;
+      if (zeroAndMinusValidation && lastValue === "-") {
+        el.value = "-";
+        return event.preventDefault();
       }
 
-      if (ifDoubleDot) {
-        el.value = valueWithoutLast;
+      if (zeroAndMinusValidation && lastValue === ".") {
+        el.value = "0.";
+        return event.preventDefault();
+      }
+
+      if (validDotAfterMinus(lastValue)) {
+        el.value = "-0.";
+        return event.preventDefault();
+      }
+
+      if (validInput(lastValue) || lastValue === " ") {
+        return event.preventDefault();
+      }
+
+      if (validDotAndMinus(lastValue)) {
+        return event.preventDefault();
       }
     });
+
+    el.addEventListener("blur", () => {
+      let lastValue = el.value[el.value.length - 1];
+
+      if (el.value === "-0" || el.value === "-0.") {
+        el.value = "";
+      } else if (lastValue === "." || lastValue === "-") {
+        el.value = el.value.slice(0, -1);
+      }
+    });
+
+    el.addEventListener("input", event => {
+      newValue = event.target.value;
+      console.log(oldValue);
+
+      if (validMinus(lastIntroduced)) {
+        el.value = oldValue;
+      }
+
+      if (oldValue.includes("-") && lastIntroduced === "-") {
+        el.value = oldValue;
+      }
+
+      if (validDotDeleting(oldValue, newValue) && el.value[0] === "0") {
+        el.value = el.value.slice(1, el.value.length);
+      }
+
+      if (
+        validDotDeleting(oldValue, newValue) &&
+        el.value[0] === "-" &&
+        el.value[1] === "0"
+      ) {
+        el.value = el.value.slice(0, 1) + el.value.slice(2, el.value.length);
+      }
+
+      if (newValue[0] === ".") {
+        el.value = oldValue;
+      }
+    });
+
+    const validDotDeleting = (oldValue, newValue) => {
+      return oldValue.includes(".") && !newValue.includes(".");
+    };
+
+    const validFirstSymbols = value => {
+      return (
+        (el.value === "0" && value !== ".") ||
+        (el.value === "-0" && value !== ".")
+      );
+    };
+
+    const validZeroAndMinus = value => {
+      return (
+        (value === "." && el.value === "") || (value === "-" && el.value === "")
+      );
+    };
+
+    const validDotAfterMinus = value => {
+      return value === "." && el.value === "-";
+    };
+
+    const validInput = value => {
+      return (
+        isNaN(Number(value)) &&
+        value !== "." &&
+        value !== "-" &&
+        value !== "Backspace" &&
+        value !== "Delete" &&
+        value !== "ArrowLeft" &&
+        value !== "ArrowRight" &&
+        value !== "ArrowDown" &&
+        value !== "ArrowUp"
+      );
+    };
+
+    const validDotAndMinus = value => {
+      return value === "." && el.value.includes(".");
+    };
+
+    const validMinus = value => {
+      return value === "-" && newValue.indexOf("-") > 0;
+    };
   }
 });
 
